4a83c956ea0f9dd13386c2ddd7a3eb9a21aa0cdc
Lucasdevfrontbr <Lucasfrontdev@gmail.com>
2023-03-15 09:43:39 -0300

    .

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,9 @@
-const pass=require('./pass')
+
 module.exports = {
-    pass: 'UdY4PTxt7)!Yn+S(',
     PORT: process.env.PORT || 8000,
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || pass,
+    DB_PASSWORD: process.env.DB_PASSWORD || 'UdY4PTxt7)!Yn+S(',
     DB_NAME: process.env.DB_NAME || 'library'
   };
   
