28036abc973796eb07447bb7e64cb4b06d2f6e0a
Lucasdevfrontbr <Lucasfrontdev@gmail.com>
2023-03-15 15:42:04 -0300

    update

--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,7 @@ module.exports = {
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_USER: process.env.DB_USER || 'root',
     DB_PASSWORD: process.env.DB_PASSWORD || 'UdY4PTxt7)!Yn+S(',
-    DB_NAME: process.env.DB_NAME || 'library'
+    DB_NAME: process.env.DB_NAME || 'library',
+      DB_PORT:process.env.DB_PORT 
   };
   
--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,7 @@ const connection=mysql.createConnection({
     user:DB_USER,
    password: DB_PASSWORD,
    database: DB_NAME,
+   port:DB_PORT
 })
 connection.connect((error)=>{
 if(error) throw error
